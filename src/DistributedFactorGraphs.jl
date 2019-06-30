7173264be4d076e8521409266b46575fff4e656a
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-30 01:27:10 +0000

    Snapshot of branch master taken on 2019-06-29T21:27:09.166-04:00 from "https://github.com/UnofficialJuliaMirror/DistributedFactorGraphs.jl-b5cc3c7e-6572-11e9-2517-99fb8daf2f04"

--- src/DistributedFactorGraphs.jl
+++ src/DistributedFactorGraphs.jl
@@ -6,6 +6,7 @@ using Requires
 using Dates
 using Distributions
 using Reexport
+using JSON2
 
 # Entities
 include("entities/AbstractTypes.jl")
@@ -13,23 +14,60 @@ include("entities/DFGFactor.jl")
 include("entities/DFGVariable.jl")
 
 export AbstractDFG
+export AbstractParams, NoSolverParams
 export DFGNode
+
 export DFGFactor
+export InferenceType, PackedInferenceType, FunctorInferenceType, InferenceVariable, ConvolutionObject
+
 export DFGVariable
 export label, timestamp, tags, estimates, estimate, solverData, solverDataDict, id, smallData, bigData
 export setSolverData
 export label, data, id
 
 # Solver (IIF) Exports
-export VariableNodeData, PackedVariableNodeData
+export VariableNodeData, PackedVariableNodeData, VariableEstimate
 export GenericFunctionNodeData#, FunctionNodeData
 export getSerializationModule, setSerializationModule!
 export pack, unpack
 
-# Include the Graphs.jl API.
-include("services/GraphsDFG.jl")
 include("services/AbstractDFG.jl")
 include("services/DFGVariable.jl")
+
+# Include the Graphs.jl API.
+include("GraphsDFG/GraphsDFG.jl")
+
+function __init__()
+    @require DataFrames="a93c6f00-e57d-5684-b7b6-d8193f3e46c0" begin
+        if isdefined(Main, :DataFrames)
+            """
+                $(SIGNATURES)
+            Get an adjacency matrix for the DFG as a DataFrame.
+            Rows are all factors, columns are all variables, and each cell contains either nothing or the symbol of the relating factor.
+            The first column is the factor headings.
+            """
+            function getAdjacencyMatrixDataFrame(dfg::GraphsDFG)::Main.DataFrames.DataFrame
+                varLabels = sort(map(v->v.label, getVariables(dfg)))
+                factLabels = sort(map(f->f.label, getFactors(dfg)))
+                adjDf = DataFrames.DataFrame(:Factor => Union{Missing, Symbol}[])
+                for varLabel in varLabels
+                    adjDf[varLabel] = Union{Missing, Symbol}[]
+                end
+                for (i, factLabel) in enumerate(factLabels)
+                    push!(adjDf, [factLabel, DataFrames.missings(length(varLabels))...])
+                    factVars = getNeighbors(dfg, getFactor(dfg, factLabel))
+                    map(vLabel -> adjDf[vLabel][i] = factLabel, factVars)
+                end
+                return adjDf
+            end
+        end
+    end
+
+  @require Neo4j="d2adbeaf-5838-5367-8a2f-e46d570981db" begin
+    # Include the Cloudgraphs API
+    include("CloudGraphsDFG/CloudGraphsDFG.jl")
+  end
+end
 
 # not sure where to put
 include("Common.jl")
